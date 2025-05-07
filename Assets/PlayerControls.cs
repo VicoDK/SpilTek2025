6fb5661fa0074f50f251e853bbd04aa4a1a0f36f
Vico <Victor@r-s.nu>
2025-05-07 17:22:45 +0200

    bomb

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -2,21 +2,25 @@ using System;
 using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Networking;
 
-public class PlayerControls : MonoBehaviour
+public class PlayerControls : NetworkBehaviour
 {
     public PlayerInput controls;
     public GameObject bomb;
+    public GameObject bombe;
     public bool isAttacking = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
   
     void Update()
     {
+        if (!isLocalPlayer) return;
+
         if (controls.actions.FindAction("BombAttack").triggered && !isAttacking)
         {
-             isAttacking = true;
-            GameObject bombe = Instantiate(bomb, transform.position, Quaternion.identity);
+            isAttacking = true;
+            bombe = Instantiate(bomb, transform.position, Quaternion.identity);
             bombe.GetComponent<BomExplode>().Player = this.gameObject;
             NetworkServer.Spawn(bombe);
         }
